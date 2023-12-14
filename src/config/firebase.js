d68ecf4d41bc84983d9b79ee7e0c46762f03ee8b
zaid-kh <z.kh2800@gmail.com>
2023-12-14 11:50:32 +0200

    chore:  Integrate Firebase for hosting and future features
    🔧 Set up Firebase for website hosting. This commit lays the foundation for upcoming features, including authentication and database integration.

---
+++ src/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCvXyvmPvYfXPnYGwXM0Hl73Fd5ae4hBGU",
+  authDomain: "restaurant-react-appleseeds.firebaseapp.com",
+  projectId: "restaurant-react-appleseeds",
+  storageBucket: "restaurant-react-appleseeds.appspot.com",
+  messagingSenderId: "360103964538",
+  appId: "1:360103964538:web:e5fd6cf49cad639a29b0c3",
+  measurementId: "G-XRBD7SCD9E",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
